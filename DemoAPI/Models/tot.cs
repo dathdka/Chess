fde63e6e2b408d111f37a0050e1a69df7ce66b60
dathdka <dathdka@gmail.com>
2022-06-22 18:03:59 +0700

    loi

--- DemoAPI/Models/tot.cs
+++ DemoAPI/Models/tot.cs
@@ -5,17 +5,20 @@ using System.Web;
 
 namespace DemoAPI.Models
 {
-    public class tuong : MoveModel
+   
+    public class tot : MoveModel
     {
+        public int x { get; set; }
+        public int y { get; set; }
 
-        public tuong(string id, int x, int y)
+        public tot(string id, int x, int y)
         {
             this.x = x;
             this.y = y;
             this.id = id;
         }
-        public tuong() { }
-
+        public tot() { }
+        //check buoc di
         public MoveModel checkMove(MoveModel node)
         {
             if (isWithin(node.top) != -1 && isWithin(node.left) != -1)
