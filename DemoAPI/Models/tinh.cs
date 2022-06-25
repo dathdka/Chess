514d48e2068045d514f8792d9bd687dcc6364345
Tonight or never <vothuykg123@gmail.com>
2022-06-25 18:51:16 +0700

    update tinh (1 truong hop chua giai quyet)

--- DemoAPI/Models/tinh.cs
+++ DemoAPI/Models/tinh.cs
@@ -57,10 +57,10 @@ namespace DemoAPI.Models
                             return node;
                         }
                     //Chặn đi 1 nước
-                    if(node.x == curnode.x -2 && node.y == curnode.y + 2)
-                    {
-                        return node;
-                    }
+                    //if(node.x == curnode.x -2 && node.y == curnode.y + 2)
+                    //{
+                    //    return node;
+                    //}
                     //cấm qua sông 
                     if (curnode.isRed == true && node.x < 5)
                     {
@@ -341,10 +341,10 @@ namespace DemoAPI.Models
                         return node;
                     }
                     //Chặn đi 1 nước
-                    if (node.y == curnode.y + 1)
-                    {
-                        return node;
-                    }
+                    //if (node.y == curnode.y + 1)
+                    //{
+                    //    return node;
+                    //}
                     //cấm qua sông 
                     if (curnode.isRed == true && node.x < 5)
                     {
